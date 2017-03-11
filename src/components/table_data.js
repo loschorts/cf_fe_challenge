ba42be977467dfd9c3defaba25d471355d850d7a
Ken Tran <loschorts@gmail.com>
2017-03-10 17:08:53 -0800

    delete all files

--- src/components/table_data.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-	{type: "A", name: "mail", value: "216.70.64.90", ttl: "15 mins" },
-	{type: "MX", name: "example.websiteone.com", value: "mail.examplewebsiteone.com", ttl: "12 hours"},
-	{type: "TXT", name: "lorem ipsum", value: "v=spf1 ip4:123.123.123.123 ~all", ttl: "Automatic"},
-	{type: "AAAA", name: "examplewebsiteone.com", value: "2001:0db9:95a3:0000:0000:9a2e2983f98h723fhdjsk2198198f32f23f23f23234zs35", ttl: "Automatic"},
-	{type: "LOC", name: "lorem ipsum", value: "216.70.64.90", ttl: "1 day"},
-	{type: "SRV", name: "lorem ipsum", value: "216.70.64.90", ttl: "Automatic"},
-];
