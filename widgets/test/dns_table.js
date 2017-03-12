9db0d95aa1820b68b95c1bd62232426e863c0bf0
Ken Tran <loschorts@gmail.com>
2017-03-12 13:37:23 -0700

    dns table tests

--- widgets/test/dns_table.js
+++ widgets/test/dns_table.js
@@ -13,33 +13,68 @@ describe('DNS Table', function() {
     browser.visit('/#/dns', done);
   });
 
-  it('should mount', function() {
-    browser.assert.success();
-    browser.assert.element(".dns-table")
-  })
-
-  it('should contain table data', function(){
-    browser.assert.element('.dns-table thead');
-    browser.assert.elements('.dns-table tr', {atLeast: 1});
-  })
-
-  describe('rows', function(){
-
-    it('can be checked', function(){
-
-      const firstRow = `.dns-table-row:first-of-type`;
-      const firstCheckBox = `${firstRow} .table-checkbox`;
-      const firstRowChecked = `${firstRow} checked`
-
-      browser.assert.element(firstCheckBox);
-      // browser.assert.elements(firstRowChecked, 0);
-      browser.click(firstCheckBox);
-      browser.assert.elements(firstRowChecked, 1);
-
+  describe('renders', function() {
+    it('should mount', function() {
+      browser.assert.success();
+      browser.assert.element(".dns-table")
     })
 
-    it('can be selected', function(){
+    it('should contain table data', function(){
+      browser.assert.element('.dns-table thead');
+      browser.assert.elements('.dns-table tr', {atLeast: 1});
+    })
+  })
 
+  describe('has checkable and selectable rows', function(){
+
+    const firstRow = `.dns-table-row:first-of-type`;
+    const firstCheckBox = `${firstRow} .table-checkbox`;
+    const firstRowChecked = `${firstRow} checked`;
+    const firstRowSelected = `${firstRow} checked`;
+
+    describe('checking', function(){
+
+      it('rows can be checked', function(){
+        // check for a class toggle onClick
+
+        browser.assert.elements(firstRowChecked, 0);
+        browser.click(firstCheckBox);
+
+        // shove onto call stack to await react re-render
+        setTimeout(()=>{
+          browser.assert.elements(firstRowChecked, 1);
+        })
+
+      })
+
+      it("rows aren't selected when checked", function(){
+        // check for a class toggle onClick
+
+        browser.assert.elements(firstRowSelected, 0);
+        browser.click(firstCheckBox);
+
+        // shove onto call stack to await react re-render
+        setTimeout(()=>{
+          browser.assert.elements(firstRowSelected, 1);
+        })
+
+      })     
+    })
+
+    describe('selecting', function(){
+
+      it('rows can be selected on click', function(){
+        // check for a class toggle onClick
+
+        browser.assert.elements(firstRowSelected, 0);
+        browser.click(firstRow);
+
+        // shove onto call stack to await react re-render
+        setTimeout(()=>{
+          browser.assert.elements(firstRowSelected, 1);
+        })
+
+      })    
     })
 
 
